152302ac9c7b297d5a35d2b068a705f9d84d2fca
TheBlueStars <nguyentruongnct8@gmail.com>
2025-08-20 02:01:12 +0700

    Update ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    
     implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -40,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
